da9301113eefbca41ef2b3449e98a5e45836d4d3
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2025-01-15 08:25:11 +1300

    Added LogsService and LogsRepository dependencies to controller spec.

--- src/logs/logs.controller.spec.ts
+++ src/logs/logs.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LogsController } from './logs.controller';
+import { LogsService } from './logs.service';
+import { LogsRepository } from './repositories/logs.repository';
 
 describe('LogsController', () => {
   let controller: LogsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [LogsService, LogsRepository],
       controllers: [LogsController],
     }).compile();
 
